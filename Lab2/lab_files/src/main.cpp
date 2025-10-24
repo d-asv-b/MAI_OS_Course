644114ecb20dd6c6c478d994c5ddb251880cd320
d-asv-b <dlasvlb@gmail.com>
2025-10-24 17:35:32 +0300

    Lab 2 in dev.

--- Lab2/lab_files/src/main.cpp
+++ Lab2/lab_files/src/main.cpp
@@ -2,7 +2,47 @@
 #include <fstream>
 #include <string>
 #include <boost/multiprecision/cpp_int.hpp>
+
 using namespace boost::multiprecision;
+
+uint512_t* shared_data = nullptr;
+std::ifstream shared_input;
+
+pthread_mutex_t shared_mutex;
+
+uint512_t calc_average_value_in_batch(size_t batch_size) {
+    pthread_mutex_lock(&shared_mutex);
+    uint512_t batch_average = 0;
+
+    if (!shared_input.is_open()) {
+        std::cerr << "Файл для чтения не был открыт." << "\n";
+        return 1;
+    }
+    
+    if (shared_data == nullptr) {
+        std::cerr << "Память не была инициализирована." << "\n";
+        return 1;
+    }
+
+    std::string hex_number;
+    for (size_t i = 0; i < batch_size; ++i) {
+        shared_input >> hex_number;
+
+        if (hex_number.substr(0, 2) != "0x") {
+            hex_number = "0x" + hex_number;
+        }
+        shared_data[i] = uint512_t(hex_number);
+    }
+
+    for (size_t i = 0; i < batch_size; ++i) {
+        batch_average += batch_size;
+    }
+    batch_average /= batch_size;
+
+    pthread_mutex_unlock(&shared_mutex);
+
+    return batch_average;
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 5) {
@@ -37,8 +77,12 @@ int main(int argc, char* argv[]) {
         std::cerr << "Количество потоков должно быть больше нуля.\n";
         return 1;
     }
-    else if (memory_limit < 128) {
+    if (memory_limit < 128) {
         std::cerr << "Количество ОЗУ должно быть не меньше 128 байт.\n";
+        return 1;
+    }
+    if (memory_limit % 64 != 0) {
+        std::cerr << "Количество ОЗУ должно нацело делиться на 64 (минимум 128 байт).\n";
         return 1;
     }
 
@@ -52,11 +96,12 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    std::string hex_number;
-    while (input >> hex_number) {
-        
-    }
+    size_t count;
+    input >> count;
 
+    size_t batch_size = memory_limit / 64;
+
+    
 
     return 0;
 }
